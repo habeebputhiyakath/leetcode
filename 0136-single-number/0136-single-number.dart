13aba7d716a724849b60647b267d1deae2e3e1a5
IRFAN HABEEB <110285678+habeebputhiyakath@users.noreply.github.com>
2024-10-26 16:03:51 +0530

    Time: 268 ms (87.5%), Space: 150.7 MB (52.31%) - LeetHub

---
+++ 0136-single-number/0136-single-number.dart
@@ -0,0 +1,20 @@
+class Solution {
+  int singleNumber(List<int> a) {
+      
+int flag;
+int? n;
+  for (var i = 0; i < a.length; i++) {
+    flag = 0;
+    for (var j = 0; j < a.length; j++) {
+      if (a[i] == a[j] && i != j) {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 0) {
+    n=a[i];
+    }
+  }
+  return n!;
+  }
+}
